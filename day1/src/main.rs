350a1fd88877e950a4521207269ebf94a4f8c89c
Scott-fo <scottfo@live.com>
2022-12-01 09:04:25 +0000

    feat: day1 of advent of code

---
+++ day1/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::collections::BinaryHeap;
+
+fn main() {
+    let file_path = "puzzle-input.txt";
+    let file = fs::File::open(file_path)
+        .expect("Could not find file");
+
+    let reader = BufReader::new(file);
+    let mut max_heap = BinaryHeap::new();
+    let mut sum = 0;
+
+    for line in reader.lines() {
+        let entry = line.unwrap();
+        if entry.is_empty() {
+            max_heap.push(sum);
+            sum = 0;
+            continue;
+        } else {
+            sum = sum + entry.parse::<i64>().unwrap();
+        }
+    }
+
+    max_heap.push(sum);
+
+    // Part 1
+    // println!("Max value is: {:?}", max_heap.peek());
+
+    let top_three_sum = max_heap.pop().unwrap() + max_heap.pop().unwrap() + max_heap.pop().unwrap();
+
+    println!("Top three sum: {:?}", top_three_sum);
+}
