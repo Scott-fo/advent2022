f674087e273e152dcf7436b5d1956d05e54cfc5a
Scott-fo <scottfo@live.com>
2022-12-02 13:20:06 +0000

    refactor: rustfmt

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     let input = include_str!("../puzzle-input.txt");
     let mut lines: Vec<usize> = input
-        .split("\n\n") 
-        .map(|line| line.split("\n").flat_map(|num| num.parse::<usize>()).sum::<usize>())
+        .split("\n\n")
+        .map(|line| {
+            line.split("\n")
+                .flat_map(|num| num.parse::<usize>())
+                .sum::<usize>()
+        })
         .collect();
-        //.max(); // Instead of collect for part 1
+    //.max(); // Instead of collect for part 1
 
     lines.sort_by(|a, b| b.cmp(a));
     println!("1: {:?}, 2: {:?}, 3: {:?}", lines[0], lines[1], lines[2]);
