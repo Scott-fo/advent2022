27c57aec12aa4fc9e67b37053abd578c9b99ba45
Scott-fo <scottfo@live.com>
2022-12-02 13:14:06 +0000

    feat: take a better approach than using the heap
    Use methods available in rust to create a more compact solution than the
    for loop and binary heap implementation originally used yesterday
    
    Based on learning from better engineers discussing their approaches

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,33 +1,11 @@
-use std::collections::BinaryHeap;
-use std::fs;
-use std::io::BufRead;
-use std::io::BufReader;
-
 fn main() {
-    let file_path = "puzzle-input.txt";
-    let file = fs::File::open(file_path).expect("Could not find file");
+    let input = include_str!("../puzzle-input.txt");
+    let mut lines: Vec<usize> = input
+        .split("\n\n") 
+        .map(|line| line.split("\n").flat_map(|num| num.parse::<usize>()).sum::<usize>())
+        .collect();
+        //.max(); // Instead of collect for part 1
 
-    let reader = BufReader::new(file);
-    let mut max_heap = BinaryHeap::new();
-    let mut sum = 0;
-
-    for line in reader.lines() {
-        let entry = line.unwrap();
-        if entry.is_empty() {
-            max_heap.push(sum);
-            sum = 0;
-            continue;
-        }
-
-        sum = sum + entry.parse::<i64>().unwrap();
-    }
-
-    max_heap.push(sum);
-
-    // Part 1
-    // println!("Max value is: {:?}", max_heap.peek());
-
-    let top_three_sum = max_heap.pop().unwrap() + max_heap.pop().unwrap() + max_heap.pop().unwrap();
-
-    println!("Top three sum: {:?}", top_three_sum);
+    lines.sort_by(|a, b| b.cmp(a));
+    println!("1: {:?}, 2: {:?}, 3: {:?}", lines[0], lines[1], lines[2]);
 }
