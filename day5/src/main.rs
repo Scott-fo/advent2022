4b30d6ba618876e18263b8110c7b0373e995f37c
Scott-fo <scottfo@live.com>
2022-12-05 11:50:43 +0000

    refactor: improve structure of code

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -8,86 +8,80 @@ fn main() {
 }
 
 fn part_two(input: &str) {
-    let input: Vec<&str> = input.split("\n").collect();
-
-    let number_of_stacks = (input[0].len() + 1) / 4;
+    let number_of_stacks = (input.split_once("\n").unwrap().0.len() + 1) / 4;
     let mut stacks: Vec<VecDeque<char>> = vec![VecDeque::new(); number_of_stacks];
 
-    for element in &input {
-        if element.contains("1") {
-            break;
-        }
+    for element in input.split("\n") {
+        if element.contains("[") {
+            let bytes = element.as_bytes();
+            let mut index = 1;
 
-        let bytes = element.as_bytes();
-        let mut index = 1;
-
-        for idx in 0..number_of_stacks {
-            if bytes[index] != 32 {
-                stacks[idx].push_back(bytes[index] as char);
+            for idx in 0..number_of_stacks {
+                if bytes[index] != 32 {
+                    stacks[idx].push_back(bytes[index] as char);
+                }
+                index += 4
             }
-            index += 4
+        }
+
+        if element.contains("move") {
+            let instruction: Vec<usize> = element
+                .split(" ")
+                .filter_map(|num| num.parse::<usize>().ok())
+                .collect();
+
+            let mut item_vec: VecDeque<char> = VecDeque::new();
+            for _ in 0..instruction[0] {
+                item_vec.push_back(stacks[instruction[1] - 1].pop_front().unwrap());
+            }
+
+            item_vec.append(&mut stacks[instruction[2] - 1]);
+            stacks[instruction[2] - 1] = item_vec;
         }
     }
 
-    for instruction in &input {
-        if !instruction.contains("move") {
-            continue;
-        }
-
-        let instruction: Vec<usize> = instruction
-            .split(" ")
-            .filter_map(|num| num.parse::<usize>().ok())
-            .collect();
-
-        let mut item_vec: VecDeque<char> = VecDeque::new();
-        for _ in 0..instruction[0] {
-            item_vec.push_back(stacks[instruction[1] - 1].pop_front().unwrap());
-        }
-
-        item_vec.append(& mut stacks[instruction[2] - 1]);
-        stacks[instruction[2] - 1] = item_vec;
+    let mut result = VecDeque::new();
+    for idx in 0..number_of_stacks {
+        result.push_back(stacks[idx].pop_front().unwrap());
     }
 
-    println!("{:?}", stacks);
+    println!("Part 2: {:?}", result);
 }
 
 fn part_one(input: &str) {
-    let input: Vec<&str> = input.split("\n").collect();
-
-    let number_of_stacks = (input[0].len() + 1) / 4;
+    let number_of_stacks = (input.split_once("\n").unwrap().0.len() + 1) / 4;
     let mut stacks: Vec<VecDeque<char>> = vec![VecDeque::new(); number_of_stacks];
 
-    for element in &input {
-        if element.contains("1") {
-            break;
-        }
+    for element in input.split("\n") {
+        if element.contains("[") {
+            let bytes = element.as_bytes();
+            let mut index = 1;
 
-        let bytes = element.as_bytes();
-        let mut index = 1;
-
-        for idx in 0..number_of_stacks {
-            if bytes[index] != 32 {
-                stacks[idx].push_back(bytes[index] as char);
+            for idx in 0..number_of_stacks {
+                if bytes[index] != 32 {
+                    stacks[idx].push_back(bytes[index] as char);
+                }
+                index += 4
             }
-            index += 4
+        }
+
+        if element.contains("move") {
+            let instruction: Vec<usize> = element
+                .split(" ")
+                .filter_map(|num| num.parse::<usize>().ok())
+                .collect();
+
+            for _ in 0..instruction[0] {
+                let item = stacks[instruction[1] - 1].pop_front().unwrap();
+                stacks[instruction[2] - 1].push_front(item);
+            }
         }
     }
 
-    for instruction in &input {
-        if !instruction.contains("move") {
-            continue;
-        }
-
-        let instruction: Vec<usize> = instruction
-            .split(" ")
-            .filter_map(|num| num.parse::<usize>().ok())
-            .collect();
-
-        for _ in 0..instruction[0] {
-            let item = stacks[instruction[1] - 1].pop_front().unwrap();
-            stacks[instruction[2] - 1].push_front(item);
-        }
+    let mut result = VecDeque::new();
+    for idx in 0..number_of_stacks {
+        result.push_back(stacks[idx].pop_front().unwrap());
     }
 
-    println!("{:?}", stacks);
+    println!("Part 1: {:?}", result);
 }
