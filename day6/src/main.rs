a9bd1b1440444030c80f234ccb0a3288601fa418
Scott-fo <scottfo@live.com>
2022-12-06 08:19:02 +0000

    feat: day6 complete

---
+++ day6/src/main.rs
@@ -0,0 +1,34 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = include_str!("puzzle-input.txt");
+    let input: Vec<char> = input.chars().collect();
+
+    let mut head: usize = 14;
+    let mut tail: usize = 0;
+
+    while head < input.len() {
+        let result = check_for_repeat(&input[tail..head]);
+        if result {
+            break;
+        } else {
+            head += 1;
+            tail += 1;
+        }
+    }
+
+    println!("{:?}", &input[..head].iter().count());
+}
+
+fn check_for_repeat(slice: &[char]) -> bool {
+    let mut db = HashSet::new();
+    for num in slice.iter() {
+        if db.contains(num) {
+            return false;
+        } else {
+            db.insert(num);
+        }
+    }
+
+    true
+}
