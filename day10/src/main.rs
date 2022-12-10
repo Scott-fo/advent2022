dadbaedddcde9c2913f07d4d1e7d6e2578b9ddf2
Scott-fo <scottfo@live.com>
2022-12-10 23:28:28 +0000

    feat: slap dash approach to part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,12 +1,8 @@
-use std::collections::HashMap;
 use std::collections::VecDeque;
 
 fn main() {
     let mut queue: VecDeque<isize> = VecDeque::new();
-    let mut x: HashMap<isize, isize> = HashMap::new();
-
     let mut running_total = 1;
-    let mut target_idx: isize = 20;
 
     include_str!("puzzle-input.txt").lines().for_each(|line| {
         let instruction: Vec<&str> = line.split_whitespace().collect();
@@ -22,28 +18,29 @@ fn main() {
         }
     });
 
-    let mut cycle = 1;
+    let mut cycle = 0;
     while queue.len() > 0 {
         let val = queue.pop_front().unwrap();
+        draw_crt(running_total, cycle);
+        cycle += 1;
+
         if val != 0 {
+            draw_crt(running_total, cycle);
             cycle += 1;
         }
 
-        if cycle == target_idx.try_into().unwrap() {
-            x.insert(target_idx, running_total);
-            target_idx += 40;
-        }
-
         running_total += val;
-        cycle += 1;
-
-        if cycle == target_idx.try_into().unwrap() {
-            x.insert(target_idx, running_total);
-            target_idx += 40;
-        }
     }
+}
 
-    let sum: isize = x.iter().map(|cycle| cycle.0 * cycle.1).sum();
-
-    dbg!(&sum);
+fn draw_crt(total: isize, cycle: isize) {
+    let position = cycle % 40;
+    if cycle != 0 && position == 0 {
+        println!("")
+    }
+    if (total - 1) <= position && (total + 1) >= position {
+        print!("#")
+    } else {
+        print!(".")
+    }
 }
